bb38c603201d3601d4b14ce2a5db10a7bb707b2e
Samira Esquina <samiraesquina@gmail.com>
2020-07-12 10:01:17 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Shopping Cart</h1>
+      
+    </div>
+  );
+}
+
+export default App;

9e4954c8504b9d2f6d6c129c1877cd0429caefff
Samira Esquina <samiraesquina@gmail.com>
2020-07-13 13:56:32 -0400

    Edited Text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Cart from './components/Cart';
 function App() {
   return (
     <div className="App">
-      <h1> Shopping Cart </h1>
+      <h1> NamaSlay </h1>
       <Cart/>
     </div>
   );
